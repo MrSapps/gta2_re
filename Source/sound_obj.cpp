fc46300c302907afb40eae5e1708d259ae237bc7
Paul <github@paulsapps.com>
2021-12-25 12:00:00 +0000

    refactor Set3DSound_41A2F0

--- Source/sound_obj.cpp
+++ Source/sound_obj.cpp
@@ -44,43 +44,37 @@ void sound_obj::SetCDVol_41A270(unsigned char cdVol)
 // match
 char sound_obj::Set3DSound_41A2F0(char b3dSound)
 {
-    BYTE *p_field_1C_samp_count; // edi
-    unsigned __int8 field_1C_samp_count; // cl
-
-    if (this->field_0 != 0)
+    if (field_0 != 0)
     {
-        if (b3dSound != this->field_1D_b3d_sound)
+        if (b3dSound != field_1D_b3d_sound)
         {
-
             if (b3dSound)
             {
-                p_field_1C_samp_count = &this->field_1C_samp_count;
-                if (gWizardly_margulis_6FFF00.sub_58D820(&this->field_1C_samp_count))
+                if (gWizardly_margulis_6FFF00.sub_58D820(&field_1C_samp_count))
                 {
-                    this->field_10_samp_count2 = *p_field_1C_samp_count; // note: regswap from redundant local
-                    this->field_1D_b3d_sound = 1;
+                    field_1D_b3d_sound = 1;
+                    field_10_samp_count2 = field_1C_samp_count; // note: regswap from redundant local
                     return 1;
                 }
                 else
                 {
                     gWizardly_margulis_6FFF00.sub_58D9F0(1);
-                    this->field_1D_b3d_sound = 0;
-                    this->field_10_samp_count2 = 16;
+                    field_1D_b3d_sound = 0;
+                    field_10_samp_count2 = 16;
                     return 0;
                 }
             }
 
             if (gWizardly_margulis_6FFF00.sub_58D9F0(1))
             {
-                this->field_1D_b3d_sound = 0;
-                this->field_10_samp_count2 = 16;
+                field_1D_b3d_sound = 0;
+                field_10_samp_count2 = 16;
                 return 1;
             }
 
-            gWizardly_margulis_6FFF00.sub_58D820(&this->field_1C_samp_count);
-            field_1C_samp_count = this->field_1C_samp_count;
-            this->field_1D_b3d_sound = 1;
-            this->field_10_samp_count2 = field_1C_samp_count;
+            gWizardly_margulis_6FFF00.sub_58D820(&field_1C_samp_count);
+            field_1D_b3d_sound = 1;
+            field_10_samp_count2 = field_1C_samp_count;
             return 0;
         }
         return 1;
