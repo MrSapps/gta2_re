3ea928ce687bccfc8573ca9b846ee3068f7e11d6
Paul <github@paulsapps.com>
2021-12-25 12:00:00 +0000

    wip wizardly_margulis

--- Source/wizardly_margulis.cpp
+++ Source/wizardly_margulis.cpp
@@ -1,4 +1,5 @@
 #include "wizardly_margulis.hpp"
+#include <stdio.h>
 
 wizardly_margulis gWizardly_margulis_6FFF00;
 
@@ -6,6 +7,104 @@ S32 AILCALL AIL_digital_handle_reacquire(HDIGDRIVER drvr)
 {
     // stub
     return 0;
+}
+
+// 0x58D400
+wizardly_margulis::wizardly_margulis()
+{
+    field_1EA8_pAudioBuffer1 = 0;
+    field_1EAC_pAudioBuffer2 = 0;
+
+    memset(field_58_hSamples, 0, sizeof(field_58_hSamples));
+ 
+    field_9C_hStreams[0] = 0;
+    field_9C_hStreams[1] = 0;
+
+    memset(field_26C4_3d_sample, 0, sizeof(field_26C4_3d_sample));
+
+    field_A4_bLoaded = 0;
+
+    for (int i = 0; i < 320; i++)
+    {
+        field_A8_sdt_entries[i].field_0_offset = 0;
+        field_A8_sdt_entries[i].field_4_sample_length = 0;
+        field_A8_sdt_entries[i].field_8_playBackRate = 11025;
+        field_A8_sdt_entries[i].field_C = 0;
+        field_A8_sdt_entries[i].field_10 = 0;
+        field_A8_sdt_entries[i].field_14 = -1;
+
+    }
+
+    field_0_hDriver = 0;
+    field_1EB0_count_samples = 16;
+    field_1EB1_unknown = 2;
+    field_1EB2_3d_samp_count = 0;
+    field_26B4_env_idx = -1;
+    field_26B8_bHave_env = 0;
+    field_26C0_3d_provider = 0;
+    field_2704_float = -1.0;
+    field_2708_float = -1.0;
+    field_270C_float = -1.0;
+    field_26BC_k17 = 0;
+    field_2710_3d_provider_count = 0;
+    field_2714_bUnknown = 0;
+    
+    sub_58D620();
+
+    field_4_gtaAudioDriveLetter = 0;
+
+    char FileName[80];
+    field_5_str[80] = 0;
+
+    unsigned int driveLetter = 'C';
+
+    char driveStr[2];
+    driveStr[1] = 0;
+
+    while (driveLetter <= 'Z')
+    {
+        driveStr[0] = driveLetter;
+        strcpy(field_5_str, driveStr);
+        strcat(field_5_str, ":\\GTAUDIO\\");
+        strcpy(FileName, field_5_str);
+        strcat(FileName, "1.wav");
+        FILE* hFile = fopen(FileName, "rb");
+        if (hFile)
+        {
+            fclose(hFile);
+            field_5_str[80] = 1;
+            field_4_gtaAudioDriveLetter = driveLetter;
+            break;
+        }
+        driveLetter++;
+    }
+
+}
+
+char wizardly_margulis::sub_58D620()
+{
+    // todo
+    return 0;
+    /*
+    char v1; // bl
+    char v2; // [esp+3h] [ebp-Dh]
+    int Data; // [esp+4h] [ebp-Ch] BYREF
+    HKEY phkResult; // [esp+8h] [ebp-8h] BYREF
+    DWORD dwDisposition; // [esp+Ch] [ebp-4h] BYREF
+
+    if (RegCreateKeyExA(HKEY_LOCAL_MACHINE, "Software\\Aureal\\A3D", 0, 0, 0, 0x20006u, 0, &phkResult, &dwDisposition))
+        return 0;
+    Data = 0;
+    if (RegSetValueExA(phkResult, "SplashScreen", 0, 4u, (const BYTE *)&Data, 4u))
+        v1 = 0;
+    else
+        v1 = v2;
+    Data = 0;
+    if (RegSetValueExA(phkResult, "SplashAudio", 0, 4u, (const BYTE *)&Data, 4u))
+        v1 = 0;
+    RegCloseKey(phkResult);
+    return v1;
+    */
 }
 
 // match
--- Source/wizardly_margulis.hpp
+++ Source/wizardly_margulis.hpp
@@ -5,48 +5,53 @@
 
 struct serene_proskuriakova_0x18
 {
-    int field_0;
-    int field_4;
+    int field_0_offset;
+    int field_4_sample_length;
     int field_8_playBackRate;
     int field_C;
     int field_10;
     int field_14;
 };
 
-struct wizardly_margulis
+class wizardly_margulis
 {
+public:
+    wizardly_margulis();
+
+    char sub_58D620();
+
     HDIGDRIVER field_0_hDriver;
-    char field_4;
+    char field_4_gtaAudioDriveLetter;
     char field_5_str[83];
     int field_58_hSamples[16];
     int field_98_hSample;
     int field_9C_hStreams[2];
     char field_A4_bLoaded;
-    char field_A5;
-    char field_A6;
-    char field_A7;
-    serene_proskuriakova_0x18 field_A8[320];
+    char field_A5_pad;
+    char field_A6_pad;
+    char field_A7_pad;
+    serene_proskuriakova_0x18 field_A8_sdt_entries[320];
     void *field_1EA8_pAudioBuffer1;
     int field_1EAC_pAudioBuffer2;
     unsigned __int8 field_1EB0_count_samples;
-    unsigned __int8 field_1EB1;
+    unsigned __int8 field_1EB1_unknown;
     unsigned __int8 field_1EB2_3d_samp_count;
-    char field_1EB3;
+    char field_1EB3_pad;
     int field_1EB4_h3dProvider[256];
     const char *field_22B4_str[256];
-    int field_26B4;
-    int field_26B8;
-    int field_26BC;
+    int field_26B4_env_idx;
+    int field_26B8_bHave_env;
+    int field_26BC_k17;
     int field_26C0_3d_provider;
     int field_26C4_3d_sample[16];
-    int field_2704;
-    int field_2708;
-    int field_270C;
+    float field_2704_float;
+    float field_2708_float;
+    float field_270C_float;
     int field_2710_3d_provider_count;
-    char field_2714;
-    char field_2715;
-    char field_2716;
-    char field_2717;
+    char field_2714_bUnknown;
+    char field_2715_pad;
+    char field_2716_pad;
+    char field_2717_pad;
 
     BYTE sub_58E2A0();
 
