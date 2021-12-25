20cf5bb93ea8e1b656ffcded7a35c58c622afbf8
Paul <github@paulsapps.com>
2021-12-25 12:00:00 +0000

    wip GetScrFileName_5122D0

--- Source/frosty_pasteur_0xC1EA8.cpp
+++ Source/frosty_pasteur_0xC1EA8.cpp
@@ -6,19 +6,17 @@
 // nomatch
 void frosty_pasteur_0xC1EA8::Load_512330(const char *pScrName)
 {
-    unsigned __int16 tableSize; // [esp+4h] [ebp-4h] BYREF
-    unsigned int len; // [esp+Ch] [ebp+4h] FORCED BYREF
-
     if (!bSkip_mission_67D4E5)
     {
         strcpy(field_35C_full_scr_file_name, pScrName);
         Error_SetName_4A0770(field_35C_full_scr_file_name);
         File::Global_Open_4A7060(field_35C_full_scr_file_name);
-        len = 12000;
+        unsigned int len = sizeof(field_46C_base_pointers);
         File::Global_Read_4A71C0(field_46C_base_pointers, &len);
-        len = 65536;
+        len = sizeof(field_334C_script_data);
         File::Global_Read_4A71C0(field_334C_script_data, &len);
-        len = 2;
+        unsigned __int16 tableSize;
+        len = sizeof(tableSize);
         File::Global_Read_4A71C0(&tableSize, &len);
         len = 5118;
         File::GetRemainderSize_4A7250(field_1334C_strings, &len);
@@ -38,7 +36,32 @@ void frosty_pasteur_0xC1EA8::LoadStringTbl_5121E0(unsigned __int16 tableSize)
 // stub
 void frosty_pasteur_0xC1EA8::GetScrFileName_5122D0()
 {
+    char *slash_pos; // eax
+    char *pfullName; // esi
+    const char *after_slash_pos; // edx
+    unsigned int total_str_len; // kr04_4
+    size_t copyLen; // ecx
 
+    slash_pos = strrchr(this->field_35C_full_scr_file_name, '\\');
+    if (slash_pos)
+    {
+        pfullName = this->field_45C_scr_file_name;
+        after_slash_pos = slash_pos + 1;
+        *(DWORD *)pfullName = 0;
+        *((DWORD *)pfullName + 1) = 0;
+        pfullName[8] = 0;
+        total_str_len = strlen(slash_pos + 1) + 1;
+        copyLen = total_str_len - 1;
+        if (total_str_len != 1)
+        {
+            while (after_slash_pos[copyLen] != '.')
+            {
+                if (!--copyLen)
+                    return;
+            }
+            strncpy(pfullName, after_slash_pos, copyLen);
+        }
+    }
 }
 
 // stub
